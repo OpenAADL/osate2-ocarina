ac48f7a4735e19329719e3026a98de43335db35e
philip.alldredge@uah.edu <philip.alldredge@uah.edu>
2012-06-11 14:11:15 -0500

    Removed unused imports.

--- org.osate.ocarina/src/org/osate/ocarina/handlers/EnableBuild.java
+++ org.osate.ocarina/src/org/osate/ocarina/handlers/EnableBuild.java
@@ -4,8 +4,6 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.commands.IHandlerListener;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.osate.ocarina.Utils;
 
