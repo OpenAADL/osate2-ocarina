ecc569f0fa10d4345af89bdb57203268c8dc0df7
philip.alldredge@uah.edu <philip.alldredge@uah.edu>
2012-08-16 11:07:38 -0500

    Added license information

--- org.osate.ocarina/src/org/osate/ocarina/real/TheoremResult.java
+++ org.osate.ocarina/src/org/osate/ocarina/real/TheoremResult.java
@@ -1,3 +1,9 @@
+// Licensed under the Common Public License (CPL): http://www.eclipse.org/legal/cpl-v10.html
+//
+// Developed for the US Army under contract W31P4Q-05-AQ-0031
+// This software is US Government Unlimited Rights per DFARS
+// 252.227-7014.
+//
 package org.osate.ocarina.real;
 
 import java.util.Collections;
