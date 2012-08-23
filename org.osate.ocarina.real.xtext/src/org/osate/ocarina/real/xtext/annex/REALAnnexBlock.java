df1eed2ea2af2e16fa97ac4722fba1487cbc83d9
philip.alldredge@uah.edu <philip.alldredge@uah.edu>
2012-08-23 09:00:49 -0500

    Clarified license terms.

--- org.osate.ocarina.real.xtext/src/org/osate/ocarina/real/xtext/annex/REALAnnexBlock.java
+++ org.osate.ocarina.real.xtext/src/org/osate/ocarina/real/xtext/annex/REALAnnexBlock.java
@@ -1,9 +1,10 @@
-// Licensed under the Common Public License (CPL): http://www.eclipse.org/legal/cpl-v10.html
-//
-// Developed for the US Army under contract W31P4Q-05-AQ-0031
-// This software is US Government Unlimited Rights per DFARS
-// 252.227-7014.
-//
+/*
+* Use of the OSATE2-Ocarina plugin is subject to the terms of the license set forth
+* at http://www.eclipse.org/legal/cpl-v10.html.
+*
+* Note: Contributions by user philip.alldredge@uah.edu were performed under contract W31P4Q-05-A-031 for the US Army. 
+* These contributions are covered by US Government Unlimited Rights per DFARS 252.227-7014. You are entitled to use this software under the conditions of the Common Public License.
+*/
 package org.osate.ocarina.real.xtext.annex;
 
 import org.osate.ocarina.real.xtext.real.Model;
