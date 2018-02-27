fdfc72449cc713fa209e87a9cac23b7499ea96d8
yoogx <hugues.jerome@gmail.com>
2018-02-27 21:33:15 +0100

    * Remove useless imports

--- org.osate.ocarina/src/org/osate/ocarina/util/FileHelper.java
+++ org.osate.ocarina/src/org/osate/ocarina/util/FileHelper.java
@@ -1,7 +1,5 @@
 package org.osate.ocarina.util;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryIteratorException;
